8ef2f3777b909bfa0aba4bc974335ebeac05b28d
Gustavo Daniel Carmona <p8queen@gmail.com>
2024-09-11 00:35:23 -0300

    primeras asignaciones.

--- APP.py
+++ APP.py
@@ -1,1 +1,14 @@
-print ('Hola Mundo')
+def generarMenu():
+    # desarrolla joaquin
+    print("1. ...")
+    print("2. ...")
+
+def crearDiccionario():
+    diccionario = {}
+    # desarrolla pablo 
+    # leer data.csv y crear como en modelo-diccionario.json
+    return diccionario
+
+def mostrarGastos(mes):
+    # desarrolla gustavo
+    pass
