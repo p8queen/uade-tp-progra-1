9e98e84c4957c681d6694767ddd11dd1552a8660
Gustavo Daniel Carmona <p8queen@gmail.com>
2024-09-11 08:34:15 -0300

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,3 +1,7 @@
+tupla = ('enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre')
+matriz = []
+diccionario = {} 
+
 def generarMenu():
     # desarrolla joaquin
     print("1. ...")
@@ -12,3 +16,39 @@ def crearDiccionario():
 def mostrarGastos(mes):
     # desarrolla gustavo
     pass
+
+def gastosPorMes(mes):
+    # desarrolla gustavo
+    pass
+
+def gastosPorCategoria(mes):
+    # desarrolla gustavo
+    pass
+
+def gastosPorCategoria(categoria):
+    # desarrolla gustavo
+    # return lista de gastos
+    pass
+
+def gastosPorMesCategoria(mes, categoria):
+    # desarrolla gustavo
+    # return lista de gastos
+    pass
+
+def totalMes(mes):
+    # desarrolla gustavo
+    # return total de gastos
+    pass
+
+def totalMesCategoria(mes, categoria):
+    # desarrolla gustavo
+    # return total de gastos mes y categoria
+    pass
+
+def listaDeCategorias():
+    # desarrolla gustavo
+    categorias = [x[2] for x in matriz]
+    return list(set(categorias)) # elimina duplicados
+    
+# CRUD Gastos
+# crear, recuperar, actualizar, borrar
