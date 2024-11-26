125327ec4c60f1c2296016fb05a98fbcb520d53e
Pablo Quiñonez <pablo-.quinonez@hotmail.com>
2024-11-26 18:39:37 -0300

    Update test_funciones.py

--- app_v4_final/test_funciones.py
+++ app_v4_final/test_funciones.py
@@ -1,4 +1,4 @@
-from funciones import totalGastosPorMes, totalGastosPorCategoria, mostrarGastosPorMes
+from funciones import totalGastosPorMes, totalGastosPorCategoria, mostrarGastosPorMes, obtenerCategoriasEnUso
 
 def test_totalGastosPorMes():
     diccionarioGastos = {'Enero': {'Alquiler': [1000], 'Comida': [500], 'Ropa': [200]},
@@ -15,17 +15,27 @@ def test_totalGastosPorCategoria():
                     [13, (2024, 1, 1), 1000.8, 'Salud', True],
                     [14, (2024, 12, 9), 700.0, 'Entretenimiento', True],
                     [15, (2024, 11, 11), 300.0, 'Servicios', True],
-                    [16, (2024, 10, 24), 9.20, 'Alimentación', True]]
+                    [16, (2024, 10, 24), 9.25, 'Alimentación', True]]
     resultado = totalGastosPorCategoria(matrizGastos)
     # Assert
-    assert resultado == 2010.0
+    assert resultado == 2010.05
 
 def test_mostrarGastosPorMes():
-    diccionarioGastos = {'Enero': {'Alquiler': [1000], 'Comida': [500], 'Ropa': [200]},
+    diccionarioGastos = {'Enero': {'Alquiler': [1000], 'Comida': [300], 'Ropa': [200]},
                          'Febrero': {'Alquiler': [1000], 'Comida': [500], 'Ropa': [500]},
                          'Marzo': {'Alquiler': [1000], 'Comida': [100], 'Ropa': [200]} }
     
-    assert mostrarGastosPorMes(diccionarioGastos, 'Enero') == 1700.0
+    assert mostrarGastosPorMes(diccionarioGastos, 'Enero') == 1500.0
     assert mostrarGastosPorMes(diccionarioGastos, 'Febrero') == 2000.0
     assert mostrarGastosPorMes(diccionarioGastos, 'Marzo') == 1300.0
+    
+def test_obtenerCategoriasEnUso():
+    matrizGastos = [[12, (2024, 8, 7), 123.0, 'Alimentación', False], 
+                    [13, (2024, 1, 1), 1000.8, 'Salud', True],
+                    [14, (2024, 12, 9), 700.0, 'Entretenimiento', True],
+                    [15, (2024, 11, 11), 300.0, 'Servicios', True],
+                    [16, (2024, 10, 24), 9.25, 'Alimentación', True]]
+    resultado = obtenerCategoriasEnUso(matrizGastos)
+    # Assert
+    assert list(resultado) == ['Salud', 'Entretenimiento', 'Servicios', 'Alimentación']
     
